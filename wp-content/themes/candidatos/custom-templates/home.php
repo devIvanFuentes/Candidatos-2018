017e9a761ae9c72709cbb15a85c79575e555bbfb
Ivan <ivanfuentesglez@gmail.com>
2018-01-31 23:15:02 -0600

    Configuracion de estilos y scripts
    Se agregaron los estilos y scripts, y se agrego el diseño de las tarjetas donde se muestra informacion de cada candidato

---
+++ wp-content/themes/candidatos/custom-templates/home.php
@@ -0,0 +1,55 @@
+<?php 
+	/* Template Name: Home */ 
+	get_header( 'custom' );
+
+?>
+
+<div class="row">
+	<div class="col s12">
+		<h4 class="center-align">Conoce a los candidatos</h4>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col s12 m6 l3">
+			
+			<div class="candidate hoverable">
+				<div class="candidate__image"></div>
+				<div class="candidate__description">
+					<p class="candidate__party">Morena</p>
+					<a href="" class="candidate__title">Andres Manuel López Obrador</a>
+				</div>
+			</div>
+		
+		</div>
+
+		<div class="col s12 m6 l3">
+			
+			<div class="candidate hoverable">
+				<div class="candidate__image"></div>
+			</div>
+		
+		</div>
+
+		<div class="col s12 m6 l3">
+			
+			<div class="candidate hoverable">
+				<div class="candidate__image"></div>
+			</div>
+		
+		</div>
+
+		<div class="col s12 m6 l3">
+			
+			<div class="candidate hoverable">
+				<div class="candidate__image"></div>
+			</div>
+		
+		</div>
+			
+	</div>
+</div>
+<?php  
+get_footer( 'custom' );
+?>
---
+++ wp-content/themes/candidatos/header-custom.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package candidatos
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<header id="masthead" class="site-header">
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#!" class="brand-logo">
+					<?php the_custom_logo(  ); ?>
+				</a>
+				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+				
+				<ul class="right hide-on-med-and-down">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">Javascript</a></li>
+					<li><a href="mobile.html">Mobile</a></li>
+      			</ul>
+      
+      			<ul class="side-nav" id="mobile-demo">
+					<li><a href="sass.html">Sass</a></li>
+					<li><a href="badges.html">Components</a></li>
+					<li><a href="collapsible.html">Javascript</a></li>
+					<li><a href="mobile.html">Mobile</a></li>
+      			</ul>
+    		</div>
+  		</nav>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
